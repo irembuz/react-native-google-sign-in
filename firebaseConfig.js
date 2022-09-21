022095984f4f27dc09f12d568b2099ea66bbdcfa
irembuz <irembuz7@gmail.com>
2022-09-21 10:20:55 +0300

    implemented react native dotenv on firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,15 +3,24 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/functions";
 
+import {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+} from "@env";
+
 // Your web app's Firebase configuration
-console.log(`apiKey: ${process.env.REACT_APP_API_KEY}`);
+console.log(`apiKey: ${REACT_APP_API_KEY}`);
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
